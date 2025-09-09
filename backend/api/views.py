102ac01aaa9219400d6817d447c9e41985e68a82
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-09 22:32:58 +0100

    Django Model Instance to Dictionary

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 import json
-from django.http import JsonResponse
-
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
 from products.models import Product
 
 
@@ -8,15 +8,9 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['description'] = model_data.description
-        data['price'] = model_data.price
-        #model instance (model_data)
+        data = model_to_dict(model_data) # data = model_to_dict(model_data, fields=["id", "title", "price"])
+    return JsonResponse(data)    
         
-        
-        #turn a Python dict
-        
-        
-        # return JSON to my client
-    return JsonResponse(data)
+    #     print(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={"Content-Type": "application/json"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,4 +6,8 @@ endpoint = "http://localhost:8000/api/"
 # REST API HTTP Request -> JSON (JavaScript Object Notation)
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hellow World"}) # API (Application Programming Interface) -> Web Api
-print(get_response.json()) # print raw text response
+print(get_response.headers)
+print(get_response.text)
+
+
+# print(get_response.json()) # print raw text response
