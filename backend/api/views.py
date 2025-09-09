35c1508634538820168c60db1b6ce9b738bda420
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-09 22:07:35 +0100

    Echo GET DATA

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,19 @@
+import json
 from django.http import JsonResponse
 
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello this your Django API response"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body 
+    print(request.GET) #url query params
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # string of JSON data -> Python Dict
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers) # request.META
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,5 +5,5 @@ endpoint = "http://localhost:8000/api/"
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON (JavaScript Object Notation)
 
-get_response = requests.get(endpoint) # API (Application Programming Interface) -> Web Api
-print(get_response.json()['message']) # print raw text response
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hellow World"}) # API (Application Programming Interface) -> Web Api
+print(get_response.json()) # print raw text response
