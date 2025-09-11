db0c3bd4f2ab3f8d38819709f6e444146f581931
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-11 20:35:10 +0100

    Rest Framework View & Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,15 @@
 import json
-from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from products.models import Product
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data) # data = model_to_dict(model_data, fields=["id", "title", "price"])
-    return JsonResponse(data)    
-        
-    #     print(data)
-    #     json_data_str = json.dumps(data)
-    # return HttpResponse(json_data_str, headers={"Content-Type": "application/json"})
+        data = model_to_dict(model_data, fields=["id", "title", "price"])
+    return Response(data)    
+    
--- backend/products/models.py
+++ backend/products/models.py
@@ -5,3 +5,5 @@ class Product(models.Model):
     title = models.CharField(max_length=120) # max_length = required
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=15, default=99.99)
+    
+    
