e4108afc8498411e9759818aa479df823b6e63a9
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-11 21:00:46 +0100

    Django Rest Framework View & Response

--- backend/products/models.py
+++ backend/products/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length=120) # max_length = required
+    title = models.CharField(max_length=120)  # max_length is required
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=15, default=99.99)
-    
-    
+
+    @property
+    def sale_price(self):
+        return "%.2f" % (float(self.price) * 0.8)
+
+    def get_discount(self):
+        return float(self.price) * 0.2  # example: 20% discount
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
